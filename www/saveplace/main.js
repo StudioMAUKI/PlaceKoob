4b07d4b251bf980d0860d99537f8d8d290e9e255
hoonja <hoonja@gmail.com>
2016-04-04 18:29:46 +0900

    1. URL 저장   - 클립보드 읽어와서 URL paste()   - URL 저장 레이아웃 구성 완성   - open graph 읽어오기 위한 사전 작업 완료

--- www/saveplace/main.js
+++ www/saveplace/main.js
@@ -1,9 +1,32 @@
 'use strict';
 
 angular.module('placekoob.controllers')
-.controller('saveModalCtrl', ['$scope', '$ionicModal', '$cordovaCamera', '$cordovaImagePicker', '$ionicPopup', 'PlaceManager', 'CacheService', function($scope, $ionicModal, $cordovaCamera, $cordovaImagePicker, $ionicPopup, PlaceManager, CacheService) {
+.controller('saveModalCtrl', ['$scope', '$ionicModal', '$cordovaCamera', '$cordovaImagePicker', '$ionicPopup', '$http', 'PlaceManager', 'CacheService', '$cordovaClipboard', function($scope, $ionicModal, $cordovaCamera, $cordovaImagePicker, $ionicPopup, $http, PlaceManager, CacheService, $cordovaClipboard) {
 	var saveModal = this;
 	saveModal.images = [];
+	saveModal.URL = '';
+
+	if (ionic.Platform.isIOS() || ionic.Platform.isAndroid()) {
+		$cordovaClipboard.paste()
+		.then(function(result) {
+			var pastedURL = result;
+			if (pastedURL !== '') {
+				// var confirmPopup = $ionicPopup.confirm({
+				// 	title: '클립보드에 저장한 내용이 있습니다',
+				// 	template: '클립보드의 URL을 붙여넣기 하시겠습니까?'
+				// });
+				//
+				// confirmPopup.then(function(result) {
+				// 	if(result) {
+						saveModal.URL = pastedURL;
+				// 	}
+				// });
+			}
+
+		}, function(err) {
+			console.error('Clipboard paste error : ' + error);
+		});
+	}
 
 	saveModal.savePosition = function() {
 		$ionicModal.fromTemplateUrl('saveplace/saveplace.html', {
@@ -32,6 +55,7 @@ angular.module('placekoob.controllers')
 		saveModal.saveDlg.remove();
 		saveModal.images = [];
 		saveModal.note = '';
+		saveModal.URL = '';
 	};
 
 	saveModal.confirmSave = function() {
@@ -42,6 +66,10 @@ angular.module('placekoob.controllers')
 			note: saveModal.note,
 			coords: curPos
 		});
+		saveModal.closeSaveDlg();
+	};
+
+	saveModal.confirmSaveURL = function() {		
 		saveModal.closeSaveDlg();
 	};
 
@@ -107,6 +135,46 @@ angular.module('placekoob.controllers')
 				saveModal.images.splice(index, 1);
 			}
 		});
+	}
+
+	saveModal.getURPreview = function() {
+		if (saveModal.URL === '') {
+			console.warn("URL must be valid value.");
+			return;
+		}
+		if (!saveModal.URL.startsWith('http://') && !saveModal.URL.startsWith('https://')) {
+			saveModal.URL = 'http://' + saveModal.URL;
+		}
+
+		// 브라우저 모드에서는 CORS 문제로 다른 도메인 호출이 기본적으로 안되기 때문에 테스트를 위해 분기를 시킴
+		var reqURL = saveModal.URL;
+		// test in web-browser
+		if (!ionic.Platform.isIOS() && !ionic.Platform.isAndroid()) {
+			console.log('Test in browser-mode');
+			reqURL = 'naver/PostView.nhn?blogId=mardukas&logNo=220647764523&redirect=Dlog&widgetTypeCall=true';
+		}
+
+		// 참고
+		// /mardukas/220647764523
+		// /PostView.nhn?blogId=mardukas&logNo=220647764523&redirect=Dlog&widgetTypeCall=true
+		// 일단 감 잡았으.
+		// 1. 네이버 블로그이냐 아니냐 판단 -> 맞을 경우 URL 변환
+		// 2. open graph tag 탐색
+		// 3. 성공한 경우 뿌려줌. 그렇지 않은 경우 별도로 실패 로그를 남기고 왜 그런치 추후 파악할 수 있도록 함
+		// 4. 요 로직은 결국 서버에 둬야 함
+
+		$http({
+			method: 'GET',
+			url: reqURL
+		}).
+		then(function(result) {
+			console.log(result.data);
+			var url = /<meta property="og:url"[\s]+content[\s]*=[\s]*['"][\S]+['"][\s]*\/>/i;
+			console.log('URL content: ' + url.exec(result.data));
+		}, function(err) {
+			console.error(JSON.stringify(err));
+		});
+		console.log('URL : ' + saveModal.URL);
 	}
 }])
 .controller('mainCtrl', ['$ionicPopup', 'uiGmapGoogleMapApi', 'MapService', 'placeListService', 'CacheService', function($ionicPopup, uiGmapGoogleMapApi, MapService, placeListService, CacheService) {
