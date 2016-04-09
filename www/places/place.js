9e7da4c3d07b382a8c047a632caf94b05d802d9a
hoonja <hoonja@gmail.com>
2016-04-09 11:37:56 +0900

    1. 상세 페이지 UI 조정 2. 실제 이미지 로딩하도록 수정

--- www/places/place.js
+++ www/places/place.js
@@ -5,6 +5,8 @@ angular.module('placekoob.controllers')
   var place = this
   place.place_id = parseInt($stateParams.place_id);
   console.log('Place ID : ' + place.place_id);
+  place.postHelper = PostHelper;
+
   RemoteAPIService.getPost(place.place_id)
   .then(function(post) {
       place.post = post;
@@ -12,78 +14,17 @@ angular.module('placekoob.controllers')
         place.post.tags = PostHelper.getTagsFromString(place.post.placePost.notes[0].content);
   }, function(err) {
     $ionicPopup.alert({
-      title: 'ERROR',
-      template: err
+      title: '죄송합니다!',
+      template: '해당하는 장소 정보를 불러올 수 없습니다.'
     })
     .then(function() {
       $ionicHistory.goBack();
     });
   });
 
-  // place.post = {
-  //   userPost: {
-  //     name: null,
-  //     notes: [{
-  //       content: '오늘 저녁',
-  //       timestamp: 1460108050939,
-  //       uuid: '7711D424F00D571A612ADE02A970FC4D.stxt'
-  //     }],
-  //     place_id: 43,
-  //     lps: [],
-  //     phone: null,
-  //     lonLat: {
-  //       lat: 37.400294,
-  //       timestamp: 1460108050939,
-  //       lon: 127.10329
-  //     },
-  //     addrs: [{
-  //       content: '테스트 주소(경기도 성남시 분당구 삼평동)',
-  //       timestamp: 1460108050938,
-  //       uuid: 'E9194769720F3B7CB80EA572770AE9E5.stxt'
-  //     }],
-  //     urls: [],
-  //     images: [{
-  //       note: null,
-  //       timestamp: 1460108050939,
-  //       content: '/media/images/2016/04/08/image_cSiMxbm.jpg',
-  //       uuid: '03069839F264FC9F046DD87520C4933F.img'
-  //     }],
-  //     posDesc: null
-  //   },
-  //   placePost: {
-  //     name: null,
-  //     notes: [{
-  //       content: '오늘 저녁 #판교맛집 #물냉면 #온반 #요새냉면왜이리비싸',
-  //       timestamp: 1460108050939,
-  //       uuid: '7711D424F00D571A612ADE02A970FC4D.stxt'
-  //     }],
-  //     place_id: 43,
-  //     lps: [],
-  //     phone: '010-8430-4463',
-  //     lonLat: {
-  //       lat: 37.400294,
-  //       timestamp: 1460108050939,
-  //       lon: 127.10329
-  //     },
-  //     addrs: [{
-  //       content: '테스트 주소(경기도 성남시 분당구 삼평동)',
-  //       timestamp: 1460108050938,
-  //       uuid: 'E9194769720F3B7CB80EA572770AE9E5.stxt'
-  //     }],
-  //     urls: [],
-  //     images: [{
-  //       note: null,
-  //       timestamp: 1460108050939,
-  //       content: '/media/images/2016/04/08/image_cSiMxbm.jpg',
-  //       uuid: '03069839F264FC9F046DD87520C4933F.img'
-  //     }],
-  //     posDesc: null
-  //   },
-  //   place_id: 43,
-  //   created: 1460108050970,
-  //   modified: 1460108050970
-  // };
-
+  place.deletePlace = function() {
+    console.warn('Post delte : Not yet implemented.');
+  }
 
   place.goBack = function() {
     console.log("Move Back");
