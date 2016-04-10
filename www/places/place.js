d56ab2c4ff9eae1ddee8ae9dc821275d62a2ee85
hoonja <hoonja@gmail.com>
2016-04-11 00:48:21 +0900

    1. 상세 정보 페이지에 URL 추가 기능 삽입

--- www/places/place.js
+++ www/places/place.js
@@ -9,26 +9,28 @@ angular.module('placekoob.controllers')
   place.zoomMin = 1;
   place.ImagesForSlide = [];
 
-  RemoteAPIService.getPost(place.place_id)
-  .then(function(post) {
-      place.post = post;
-      if (place.post.placePost) {
-        place.post.tags = PostHelper.getTagsWithContent(place.post.placePost.notes[0].content);
-      }
-      if (place.post.userPost.images) {
-        for (var i = 0; i < place.post.userPost.images.length; i++) {
-            place.ImagesForSlide.push(place.post.userPost.images[i].content);
+  place.loadPlaceInfo = function() {
+    RemoteAPIService.getPost(place.place_id)
+    .then(function(post) {
+        place.post = post;
+        if (place.post.placePost) {
+          place.post.tags = PostHelper.getTagsWithContent(place.post.placePost.notes[0].content);
         }
-      }
-  }, function(err) {
-    $ionicPopup.alert({
-      title: '죄송합니다!',
-      template: '해당하는 장소 정보를 불러올 수 없습니다.'
-    })
-    .then(function() {
-      $ionicHistory.goBack();
+        if (place.post.userPost.images) {
+          for (var i = 0; i < place.post.userPost.images.length; i++) {
+              place.ImagesForSlide.push(place.post.userPost.images[i].content);
+          }
+        }
+    }, function(err) {
+      $ionicPopup.alert({
+        title: '죄송합니다!',
+        template: '해당하는 장소 정보를 불러올 수 없습니다.'
+      })
+      .then(function() {
+        $ionicHistory.goBack();
+      });
     });
-  });
+  }
 
   place.deletePlace = function() {
     console.warn('Post delte : Not yet implemented.');
@@ -66,4 +68,57 @@ angular.module('placekoob.controllers')
       $ionicSlideBoxDelegate.enableSlide(false);
     }
   };
+
+  place.addUrl = function() {
+    // An elaborate, custom popup
+    var myPopup = $ionicPopup.show({
+      template: '<input type="text" ng-model="place.Url">',
+      title: '추가할 URL을 입력하세요',
+      subTitle: '붙여넣기를 하시면 편리합니다.',
+      scope: $scope,
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: '<b>확인</b>',
+          type: 'pk-accent',
+          onTap: function(e) {
+            if (!$scope.place.Url) {
+              //don't allow the user to close unless he enters wifi password
+              e.preventDefault();
+            } else {
+              return $scope.place.Url;
+            }
+          }
+        }
+      ]
+    });
+
+    myPopup.then(function(Url) {
+      console.log('Tapped!', Url);
+      RemoteAPIService.sendUserPost({
+        urls: [{
+          content: Url
+        }],
+        place_id: place.place_id
+      })
+      .then(function(result) {
+        console.log("Sending user post successed.");
+        $ionicPopup.alert({
+          title: 'SUCCESS',
+          template: 'URL이 추가로 저장되었습니다.'
+        })
+        .then(function(result){
+          place.loadPlaceInfo();
+        });
+      }, function(err) {
+        console.error("Sending user post failed.");
+        $ionicPopup.alert({
+          title: 'ERROR: Add URL',
+          template: JSON.stringify(err)
+        });
+      });
+    });
+  };
+
+  place.loadPlaceInfo();
 }]);
