c9a144a1f95d2321c7a72d0091d273f6cdad9cea
hoonja <hoonja@gmail.com>
2016-05-02 15:18:23 +0900

    저장 후 저장된 장소로 포커스 이동 및 선택 상태로

--- www/saveplace/savemodal.js
+++ www/saveplace/savemodal.js
@@ -101,6 +101,9 @@ angular.module('placekoob.controllers')
 				addr3: { content: StorageService.get('addr3') || null },
 			})
 			.then(function(result) {
+				console.log('Post sended:');
+				console.dir(result);
+				StorageService.set('last_uplace_id', result.data.uplace_uuid);
 				$ionicLoading.hide();
 				saveModal.closeSaveDlg();
 				$scope.$emit('posts.request.refresh');
